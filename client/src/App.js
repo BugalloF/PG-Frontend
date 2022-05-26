84988da5793cc5f33220fc2a2731472d2ed79286
DemianFerreyra <demiancra@gmail.com>
2022-05-26 19:53:25 -0300

    ahora si arreglao

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { PostPage } from './containers/postpage/postpage';
 import  FeedPage  from './containers/feedpage/FeedPage';
 import { PostForm } from './components/postform/postform';
 import  LoginPage  from './containers/loginpage/LoginPage';
+import  {LandingPage}  from './containers/landing/LoginPage';
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
@@ -14,9 +15,7 @@ function App() {
         <Route exact path={'/'} element={<FeedPage/>}/>
         <Route exact path={'/post/:IdPost'} element={<PostPage/>}/>
         <Route exact path={'/create'} element={<PostForm/>}/>
-        <Route exact path={'/'} element={[<LandingPage/>, <NavBar/>]}/>
         <Route exact path={'/post/:IdPost'} element={[<PostPage/>, <NavBar/>]}/>
-        <Route exact path={'/feed'} element={[<FeedPage/>,<NavBar/>]}/>
         <Route exact path={'/login'} element={<LoginPage/>}/>
       </Routes>
     </div>
