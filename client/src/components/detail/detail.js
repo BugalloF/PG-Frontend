99d62048e02c1bc16aedffcbd7d8d07c807d79a5
Fermin Bugallo <bugallof@gmail.com>
2022-06-08 11:52:57 -0300

    Falta estilizar

--- client/src/components/detail/detail.js
+++ client/src/components/detail/detail.js
@@ -1,29 +1,34 @@
 // Dependencies
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUser, faCartShopping, faHeart} from "@fortawesome/free-solid-svg-icons";
+import { faHeart} from "@fortawesome/free-solid-svg-icons";
 // Files
 import {ImageProfile} from "../imageprofile/imageprofile";
 import Paypal from "../paypal/paypal";
 import s from "./detail.module.css";
 import { useDispatch } from "react-redux";
-import axios from "axios";
-import { addLike } from "../../redux/actions";
+import { addLike, deleteLike } from "../../redux/actions";
 
 export function Detail(props) {
   const dispatch = useDispatch();
   const loggedUser = window.localStorage.getItem("userData");
   const userDataJson = JSON.parse(loggedUser);
-  console.log('aaaaaaaa',typeof(userDataJson))
+  // console.log('aaaaaaaa',typeof(userDataJson))
 
   
-  async function handlerOnClick (e){
+   function handlerOnClick (e){
     e.preventDefault()
-    dispatch(addLike(props.isLogged,props.idPost))
+    dispatch(addLike(loggedUser,props.idPost))
+    // console.log('me apreto')
   }
-  console.log('PROPSSSSSS',props.isLogged)
-  console.log('Userdatajson',userDataJson)
-
+  function handlerOnDelete (e){
+    e.preventDefault()
+    dispatch(deleteLike(loggedUser,props.idPost))
+    // console.log('me apreto')
+  }
+  // console.log('PROPSSSSSS',props.isLogged)
+  // console.log('Userdatajson',userDataJson)
+ 
   return(
     <div className={s.container}>
       <div className={s.container_detail}>
@@ -48,7 +53,7 @@ export function Detail(props) {
           {/* <button className={s.btn_fav}>
             <FontAwesomeIcon icon={faHeart} className={s.icon} /> Agregar a favorito
           </button> */}
-          { userDataJson!== null && props.isLiked===false ? 
+          { userDataJson!== null ? props.isLiked===false ? 
           <button onClick={handlerOnClick}>
             <span>
             <FontAwesomeIcon icon={faHeart} className={s.icon}  /> {props.likes}
@@ -57,7 +62,16 @@ export function Detail(props) {
           </button>
           :
           <div>
+            <button onClick={handlerOnDelete}>
+            <span>
+            <FontAwesomeIcon icon={faHeart} className={s.icon}  /> {props.likes}
+          </span>
 
+          </button>
+          </div>
+          :
+          <div>
+            NO ESTAS REGISTRADO PARA PONER ME GUSTAS
           </div>
           }
         </div>
