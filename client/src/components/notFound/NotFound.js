e26dd982207119ec4f90c7a298918bdc459d5d7c
Nicole-Lopez <ananicolelopez96@gmail.com>
2022-06-15 23:48:55 -0300

    cambio de imagen not found

--- client/src/components/notFound/NotFound.js
+++ client/src/components/notFound/NotFound.js
@@ -1,12 +1,12 @@
 import React from "react";
-import iconNot from './notFoundiconV5.png'
+import iconNot from './notFoundiconV8.png'
 import s from "./NotFound.module.css";
 
 export default function NotFound() {
 
 	return (
 		<div className={s.container_notFound} >
-			<img className={s.imgg} src={iconNot} alt="image not found" />
+			<img className={s.imgg} src={iconNot} alt="not found" />
 			<h2>Imagen no encontrada</h2>
 		</div>
 	);
